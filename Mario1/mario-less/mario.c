d3df229a2dce75aa58f51a363e9f7a93a043c61f
cg030 <101114890+cg030@users.noreply.github.com>
2022-03-09 16:39:12 +0000

    Wed, Mar 9, 2022, 4:39 PM +00:00

--- Mario1/mario-less/mario.c
+++ Mario1/mario-less/mario.c
@@ -8,7 +8,11 @@ int main(void)
     {
         n = get_int("Width: ");
     }
-    while (n < 1)
+    while (n < 1);
 
-    for (int i = )
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    prinftf("\n");
 }
