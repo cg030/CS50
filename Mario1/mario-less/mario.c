49388d075950cc2ad3c9e46edb5e965e0b567a7d
cg030 <101114890+cg030@users.noreply.github.com>
2022-03-09 16:15:57 +0000

    Wed, Mar 9, 2022, 4:15 PM +00:00

--- Mario1/mario-less/mario.c
+++ Mario1/mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n < 1)
+
+    for (int i = )
 }
