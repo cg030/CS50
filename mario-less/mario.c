bdb14435e6c57d0b42fbd195d75006c6ce909900
cg030 <101114890+cg030@users.noreply.github.com>
2022-03-09 17:22:39 +0000

    Wed, Mar 9, 2022, 5:22 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -12,8 +12,10 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; i++)
+        for (int j = 0; j < n; j++)
         {
-            printf("#\n");
+            printf("#");
         }
+        printf("\n");
+    }
 }
