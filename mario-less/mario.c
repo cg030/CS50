3a4db7c76ca3ff33a3cdb07c765990438122f2a6
cg030 <101114890+cg030@users.noreply.github.com>
2022-03-11 13:57:57 +0000

    Fri, Mar 11, 2022, 1:57 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -20,13 +20,13 @@ int main(void)
     for (int i = 0; i < h; i++)
     {
         //For loop to print spaces
-        for (int s = h - 1;s < n; s++)
+        for (int s = (h - 1); s < n; s--)
         {
-            // Print a brick
+            // Print a space
             printf(" ");
         }
         //For Loop to print bricks
-        for ()
+        for (int b = h)
         //Move to next row
         printf("\n");
     }
