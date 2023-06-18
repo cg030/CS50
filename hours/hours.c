16086d6e28bb5349c5c79922c0739d15c540442b
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-18 16:18:56 +0000

    Sun, Jun 18, 2023, 6:18 PM +02:00

--- hours/hours.c
+++ hours/hours.c
@@ -1,3 +1,5 @@
+// Calculator to compute the average or total number of hours spent studying for CS50
+
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -6,6 +8,7 @@ float calc_hours(int hours[], int weeks, char output);
 
 int main(void)
 {
+    // receive input
     int weeks = get_int("Number of weeks taking CS50: ");
     int hours[weeks];
 
