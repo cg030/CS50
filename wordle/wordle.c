ffe630f894ed3bfa927659bae10d82a2a1cd480d
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 15:58:39 +0000

    Mon, Jun 19, 2023, 5:58 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -114,7 +114,7 @@ int main(int argc, string argv[])
     // Print the game's result
     // TODO #7
 
-    if(check_word == 6)
+    if(check_word(guess, wor) == 6)
     {
         printf("You won!\n");
     }
