94b470549e460766975effe797b0a812ea247ba4
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 09:35:47 +0000

    Mon, Jun 19, 2023, 11:35 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,22 +10,8 @@ int main(void)
 
     string text = get_string("Text: ");
 
-    int rounded_score = (int)(cli(text) + 0.5);
-
-    printf("Words %i\n", rounded_score);
-    printf("Grade %i\n", rounded_score);
-    printf("Grade %i\n", rounded_score);
-
-    printf("Grade %i\n", rounded_score);
-
-}
-
-
-int count_letters(string text)
-{
-    int letters = 0;
-
      // create an array of numbers of the respective ascii letters
+     
     int ascii_letters[52]; // 26 upper case + 26 lowercase
 
     for (int i = 0; i < 26; i++) // upper case letters
@@ -46,6 +32,24 @@ int count_letters(string text)
         ascii_text[i] = (int)text[i];
     }
 
+    // calculate Index
+
+    float L = letters / words * 100.0; // L is the average number of letters per 100 words in the text
+    float S = sentences / words * 100.0; // S is the average number of sentences per 100 words in the text
+    float score = 0.0588 * L - 0.296 * S - 15.8;
+
+    // print score
+
+    int rounded_score = (int)(cli(text) + 0.5);
+
+    printf("Grade %i\n", rounded_score);
+
+}
+
+
+int count_letters(string text)
+{
+    int letters = 0;
 
     for (int i = 0; i < text_length; i++)
     {
@@ -57,12 +61,18 @@ int count_letters(string text)
         }
     }
     }
+    return letters;
 }
 
 int word_counts(string text)
 {
     int words = 2;
 
+    if (ascii_text[i] == 32) // count words; 32 is space in ascii
+        {
+            words++;
+        }
+        return words;
 }
 
 
@@ -71,51 +81,9 @@ float cli(string text)
 {
     int sentences = 0;
 
-    // create an array of numbers of the respective ascii letters
-    int ascii_letters[52]; // 26 upper case + 26 lowercase
-
-    for (int i = 0; i < 26; i++) // upper case letters
-    {
-        ascii_letters[i] = 65 + i;
-    }
-    for (int i = 0; i < 26; i++) // lower case letters
-    {
-        ascii_letters[i] = 97 + i;
-    }
-
-    int text_length = strlen(text);
-    int ascii_text[text_length];
-
-    // create an array that stores the ascii equivalent of the characters in the string
-    for (int i = 0; i < text_length; i++)
-    {
-        ascii_text[i] = (int)text[i];
-    }
-
-    if (!found)
-    {
-        if (ascii_text[i] == 32) // count words; 32 is space in ascii
-        {
-            words++;
-        }
-        else if (ascii_text[i] == 33 || ascii_text[i] == 46 || ascii_text[i] == 63) // count sentences
+    if (ascii_text[i] == 33 || ascii_text[i] == 46 || ascii_text[i] == 63) // count sentences
         {
             sentences++;
         }
-    }
-}
-
-
-    // calculate Index
-
-    float L = letters / words * 100.0; // L is the average number of letters per 100 words in the text
-    float S = sentences / words * 100.0; // S is the average number of sentences per 100 words in the text
-    float score = 0.0588 * L - 0.296 * S - 15.8;
-
-    return letters;
-    return words;
-    return sentences;
-    return score;
-
-
+        return sentences;
 }
