494a8797cb9163694a03a8e129b5f1a63dc73bf9
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 09:28:03 +0000

    Mon, Jun 19, 2023, 11:28 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -12,8 +12,53 @@ int main(void)
 
     int rounded_score = (int)(cli(text) + 0.5);
 
+    printf("Words %i\n", rounded_score);
+    printf("Grade %i\n", rounded_score);
     printf("Grade %i\n", rounded_score);
 
+    printf("Grade %i\n", rounded_score);
+
+}
+
+
+int count_letters(string text)
+{
+    int letters = 0;
+
+     // create an array of numbers of the respective ascii letters
+    int ascii_letters[52]; // 26 upper case + 26 lowercase
+
+    for (int i = 0; i < 26; i++) // upper case letters
+    {
+        ascii_letters[i] = 65 + i;
+    }
+    for (int i = 0; i < 26; i++) // lower case letters
+    {
+        ascii_letters[i] = 97 + i;
+    }
+
+    int text_length = strlen(text);
+    int ascii_text[text_length];
+
+    // create an array that stores the ascii equivalent of the characters in the string
+    for (int i = 0; i < text_length; i++)
+    {
+        ascii_text[i] = (int)text[i];
+    }
+
+
+    for (int i = 0; i < text_length; i++)
+    {
+    int found = 0;
+    for (int j = 0; j < 52; j++) // nested loop
+    {
+        if (ascii_text[i] == ascii_letters[j]) // count letters
+        {
+            letters++;
+            found = 1;
+            break;
+        }
+    }
 }
 
 //Calculate Coleman-Liau Index
@@ -78,7 +123,9 @@ float cli(string text)
     float S = sentences / words * 100.0; // S is the average number of sentences per 100 words in the text
     float score = 0.0588 * L - 0.296 * S - 15.8;
 
-
+    return letters;
+    return words;
+    return sentences;
     return score;
 
 
