2ce69f66edc9484ca06f68262fe165b705b97f60
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 07:38:47 +0000

    Mon, Jun 19, 2023, 9:38 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,6 +9,8 @@ int main(void)
 
     printf("%c\n", text[0]);
 
+    printf("Grade %f\n", score);
+
 }
 
 //Calculate Coleman-Liau Index
@@ -63,5 +65,7 @@ cli(string text)
     float L = letters / words * 100; // L is the average number of letters per 100 words in the text
     float S = sentences / words * 100; // S is the average number of sentences per 100 words in the text
     float score = index = 0.0588 * L - 0.296 * S - 15.8;
+
+    return score;
 
 }
