14b360b1c1baf92204a6ffe3a2b80ad800499bc0
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 15:33:58 +0000

    Mon, Jun 19, 2023, 5:33 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -146,6 +146,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
         {
             if(guess[i] == choice[j])
             {
+                score 
                 status[i] = EXACT; // 2 points
                 break;
             }
