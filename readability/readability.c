8de9735cac4420c72d9eeae47618e4ca9d2b5cc4
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-19 08:36:35 +0000

    Mon, Jun 19, 2023, 10:36 AM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -78,7 +78,7 @@ float cli(string text)
     float S = sentences / words * 100; // S is the average number of sentences per 100 words in the text
     float score = 0.0588 * L - 0.296 * S - 15.8;
 
-    return sentences;
+    return score;
 
 
 }
