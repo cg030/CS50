c899b99180f30657be8451ff719ce6c85837990f
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-26 09:59:24 +0000

    Mon, Jun 26, 2023, 11:59 AM +02:00

--- test/test.c
+++ test/test.c
@@ -6,20 +6,20 @@ int numbers[] = {10,3,5,7,9,1,8};
 // Bubble Sort
 int main(void)
 {
-    for (int j = 0; j < 6; j++)
-    {
         for(int i = 0; i < 6; i++)
         {
             if (numbers[i] > numbers[i + 1 ])
             {
-                numbers[i+1] = numbers[i];
+                numbers[i] = numbers[i] - (numbers[i] - numbers[i+1]);
+                numbers[i+1] = numbers[i+1] + (numbers[i] - numbers[i+1]);
             }
         }
-    }
+        
     printf("The elements of the array are:\n");
     for(int i = 0; i < 6; i++)
     {
         printf("%d ", numbers[i]);
     }
+    printf("\n");
 
 }
