958901d038afbb5bf62dd7880aade538729907a3
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-26 09:37:57 +0000

    Mon, Jun 26, 2023, 11:37 AM +02:00

--- test/test.c
+++ test/test.c
@@ -1,17 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int numbers[] = {4,3,5,7,9,1,8};
+int numbers[] = {10,3,5,7,9,1,8};
 
 int main(void)
 {
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < n-1; i++)
     {
         if (numbers[i] > numbers[i + 1 ])
         {
             //move i to the right
-            
         }
-        else
     }
 }
