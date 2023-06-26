a718ed53c779926556ca048fae575a8e667c45f9
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-26 09:43:58 +0000

    Mon, Jun 26, 2023, 11:43 AM +02:00

--- test/test.c
+++ test/test.c
@@ -3,6 +3,7 @@
 
 int numbers[] = {10,3,5,7,9,1,8};
 
+// Bubble Sort
 int main(void)
 {
     for (int j = 0; j < 6; j++)
@@ -11,8 +12,9 @@ int main(void)
         {
             if (numbers[i] > numbers[i + 1 ])
             {
-                //move i to the right
+                numbers[i]++;
             }
         }
     }
+    printf("Sorted Array: %i\n", numbers)
 }
