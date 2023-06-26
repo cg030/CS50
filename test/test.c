9486391b126dfc2736256026573510e5db3526a9
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-26 09:22:54 +0000

    Mon, Jun 26, 2023, 11:22 AM +02:00

---
+++ test/test.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int numbers[] = {4,3,5,7,9,1,8};
+
+int main(void)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        if (numbers[i] > numbers[i + 1 ])
+        {
+            //move i to the right
+            
+        }
+        else
+    }
+}
