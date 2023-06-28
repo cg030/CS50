ea0701cc4976092e446d13607cd0e6be4bafb0f9
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-28 08:27:29 +0000

    Wed, Jun 28, 2023, 10:27 AM +02:00

--- atoi/atoi.c
+++ atoi/atoi.c
@@ -25,56 +25,17 @@ int main(void)
 
 int convert(string input)
 {
-
-    // stop condition
-
-    if(input[strlen(input)] <= 0)
+    if (input[0] == '\0')
     {
         return 0;
     }
-    
-    // In the recursive version of convert, start with the last char and convert it into an integer value.
-    // Then shorten the string, removing the last char, and then recursively call convert using the shortened string as input,
-    // where the next char will be processed.
 
-    // last char of string
+    // int result = convert(input + 1) * 10 + (input[0] - '0');
 
-    char last_character = input[strlen(input)];
+       int result = convert(input + 1) * 10 + (input[0] - '0');
 
-    // convert char to integer
-
-    int chartoint = atoi(last_character);
-
-    // shorten string, removing the last char
-
-    input[] = input[strlen(input) - 1];
-
-    // recursively call convert using the shortened string as input, where the next char will be processed.
-
-    convert(last_character)
-
-    return chartoint;
+    return result;
 
 
-    // int n = strlen(input);
-    // int output;
-
-    // // need a stop condition
-    // if(n <= 0)
-    // {
-    //     return 0;
-    // }
-
-    // // recursive function
-    // convert(n - 1);
-
-    // // for loop implementing atoi()
-
-    // for(int i = 0; i < n; i++)
-    // {
-    //     output = atoi(input[i]);
-    // }
-
-    // return output;
 
 }
