d090c9c2878576d68a8ec8c90137a047de80f3a2
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-28 11:56:39 +0000

    Wed, Jun 28, 2023, 1:56 PM +02:00

--- temps/temps.c
+++ temps/temps.c
@@ -62,9 +62,9 @@ int main(void)
 // TODO: Sort cities by temperature in descending order using bubble sort
 void sort_cities(void)
 {
-    for(i = 0; i < NUM_CITIES - 1; i++)
+    for(int i = 0; i < NUM_CITIES - 1; i++)
     {
-        for(j = 1; j < NUM_CITIES; j++)
+        for(int j = 1; j < NUM_CITIES; j++)
         {
             if(temps[i].temp > temps[j].temp)
             {
