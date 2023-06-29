defed0d23aa8e7fd6d530b2c409b345c20d3e472
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-29 08:27:35 +0000

    Thu, Jun 29, 2023, 10:27 AM +02:00

--- max/max.c
+++ max/max.c
@@ -27,11 +27,6 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
-
-    // for loop comparing the integers in the array
-    // similar to bubble sort but without switching places
-    // simply use a condition if number is larger than every other number return that number
-
     int helper_array[n];
 
     for (int i = 0; i < n; i++)
@@ -43,7 +38,6 @@ int max(int array[], int n)
                 helper_array[i] = 1;
             }
             helper_array[i] = 0;
-            // have to check if element i is larger than all other elemtnes before next round of outer loop since it the next round will overwrite the previous round
         }
 
         for(int k = 0; k < n; k++)
@@ -55,3 +49,5 @@ int max(int array[], int n)
         }
     }
 }
+
+            // have to check if element i is larger than all other elemtnes before next round of outer loop since it the next round will overwrite the previous round
