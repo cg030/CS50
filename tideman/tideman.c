a0806aac97bace1c29e8919dbb89d20eb233c81f
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-30 10:54:38 +0000

    Fri, Jun 30, 2023, 12:54 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -142,10 +142,11 @@ void record_preferences(int ranks[])
 
     for(int i = 0 ; i < 3; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for(int j = i + 1; j < 3; j++)
         {
-            preferences[i][j]++; 
+            preferences[i][j]++;
         }
+
     }
 
 
