75df460f253313ab15c83cf0f622af39343ad86a
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-30 10:43:23 +0000

    Fri, Jun 30, 2023, 12:43 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -136,7 +136,7 @@ void record_preferences(int ranks[])
             {
                 preference[0][1]++; //add 1 to the respective index
             }
-    // ranks[i] (alice) updates preferences[i](Alice) [+1](Bob) [+1](Charlie)
+    // ranks[0] (alice) updates preferences[0][0](Alice)+0 [0][1](Bob)+1, [0][2](Charlie)+1
 
 
         // assign an index to a respective candidate/row i.e. Alice is row 0.
