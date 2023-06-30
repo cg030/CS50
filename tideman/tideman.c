6f3254fe1aba797435b48eec352400d431a76727
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-30 15:34:18 +0000

    Fri, Jun 30, 2023, 5:34 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -192,7 +192,7 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // we now have a sorted pairs array
+    // we now have a sorted pairs array but we don't know the 
     return;
 }
 
