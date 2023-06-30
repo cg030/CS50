4bb54c7c7c2ea8b4b81e657c65b437a39a312cb7
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-30 09:12:24 +0000

    Fri, Jun 30, 2023, 11:12 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -101,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
 {
     // TODO
 
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidates_count; i++)
     {
         if (strcmp(name, candidates[i]) == 0)
         {
