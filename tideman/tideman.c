40e3117da58b870a33d968296c71c46ad4ae2471
cg030 <101114890+cg030@users.noreply.github.com>
2023-06-30 09:35:09 +0000

    Fri, Jun 30, 2023, 11:35 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,6 +118,10 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
+    for(int i = 0; i < 3; i++)
+    {
+        ranks[]
+    }
     return;
 }
 
