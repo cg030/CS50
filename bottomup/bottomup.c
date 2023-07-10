da2c383b4dd3fc52463a14b5cf02c360e08d26ab
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-10 13:10:33 +0000

    Mon, Jul 10, 2023, 3:10 PM +02:00

--- bottomup/bottomup.c
+++ bottomup/bottomup.c
@@ -74,9 +74,6 @@ int main(int argc, char *argv[])
             // Read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // before writing the RGB triple change biHeight to negative
-            
-
             // Write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
         }
