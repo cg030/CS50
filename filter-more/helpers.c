cd4a28d4c1a0d3066f5f8aedd26128e20f2bf4b0
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-12 13:13:52 +0000

    Wed, Jul 12, 2023, 3:13 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -26,15 +26,49 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // pixels on the left side of the image should end up on the right, and vice versa
-    // pixel in image[0][2] --> image[0][width-2]
+    // pixel in image[i][j] --> image[0][width-j]
 
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)  // Only need to iterate over half the width
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j - 1]; // the right most pixel is at width - 1 so for j = 0, width - j would be incorrect
+            image[i][width - j - 1] = temp;
+        }
+    }
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // image[0][0] doesn't have pixels to the left so trying to access these would create a segmentation fault
+            if (i == 0 && j == 0)
+            {
+
+            }
+            else if (i == 0 && 0 < j < width - 1)
+            {
+
+            }
+            else if (0 < i < height - 1 && 0 < j < width - 1)
+
+            // first code the case for a pixel in the middle of the image
+            else if (0 < i < height - 1 && 0 < j < width - 1)
+            {
+                image[i][j].rgbtBlue = (image[i - 1][j - 1].rgbtBlue + image[i - 1][j ].rgbtBlue + image[i - 1][j + 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i][j].rgbtBlue + image[i][j + 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue) / 9;
+                image[i][j].rgbtGreen = (image[i - 1][j - 1].rgbtGreen + image[i - 1][j ].rgbtGreen + image[i - 1][j + 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i][j].rgbtGreen + image[i][j + 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen) / 9;
+                image[i][j].rgbtRed = (image[i - 1][j - 1].rgbtRed + image[i - 1][j ].rgbtRed + image[i - 1][j + 1].rgbtRed + image[i][j - 1].rgbtRed + image[i][j].rgbtRed + image[i][j + 1].rgbtRed + image[i + 1][j - 1].rgbtRed + image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed) / 9;
+            }
+
+        }
+    }
     return;
 }
 
