d03a91743a5126122cb509cac4fab7a236d7ae06
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-12 13:36:28 +0000

    Wed, Jul 12, 2023, 3:36 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -77,11 +77,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             }
             else if (0 < i < height && j == width - 1) // right edge
             {
-                image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j - 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j + 1].rgbtBlue + image[i + 1][j].rgbtBlue) / 6;
+                image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j - 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue) / 6;
+                image[i][j].rgbtGreen = (image[i][j].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j - 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen + image[i + 1][j].rgbtGreen) / 6;
+                image[i][j].rgbtRed = (image[i][j].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j - 1].rgbtRed + image[i][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed + image[i + 1][j].rgbtRed) / 6;
 
             }
             else if (i == height && j == 0) // bottom left corner
             {
+                image[i][j].rgbtBlue = (image[i][j].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue) / 4;
 
             }
             else if (i == height && 0 < j < width - 1) // bottom edge
