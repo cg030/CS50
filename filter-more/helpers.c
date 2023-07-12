e5c64dc6bb63dd428518239353c472df67557e16
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-12 10:16:43 +0000

    Wed, Jul 12, 2023, 12:16 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -6,15 +6,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     // passing by value just changes the copy so I have to alter the pixels using pointers?
 
     // iterate of each pixel; a pixel is a struct RGBTRIPLE
-    for (int i = 0; i < end of bitmap; i++)
+
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < end of bitmap; j++)
+        for (int j = 0; j < width; j++)
         {
             // calculate average of RGB values in each pixel
-            int average_value = (image[i][j].rgbtBlue + image[i][j].rgctGreen + image[i][j].rgbtRed) / 3;
+            int average_value = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
 
             image[i][j].rgbtBlue = average_value;
-            image[i][j].rgctGreen = average_value;
+            image[i][j].rgbtGreen = average_value;
             image[i][j].rgbtRed = average_value;
         }
     }
