6a0374db2815223270a4f6bb078ee5419b2ab7dc
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-12 16:58:50 +0000

    Wed, Jul 12, 2023, 6:58 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
+
+    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
+
 }
