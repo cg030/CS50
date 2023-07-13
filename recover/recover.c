6797f285a7f040f28c0802e14dfc5fa87c9036c6
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-13 08:52:00 +0000

    Thu, Jul 13, 2023, 10:52 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,47 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+// Define BYTE as a type for 8-bit unsigned integer
+typedef uint8_t BYTE;
+
+// Define constants for block size and file name size
+#define BLOCK_SIZE 512
+#define FILE_NAME_SIZE 8
 
 int main(int argc, char *argv[])
 {
-    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
-    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
+    // Check if the correct number of command-line arguments is provided
     if (argc != 2)
     {
-        printf("Improper usage; Only one command line argumment\n");
-        return(1);
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
     }
 
-    // Open input file
-    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
-    // Remember filenames
-    char *infile = argv[1];
-
-    // Open input file
-    FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
+    // Try to open the file
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
     {
-        printf("Could not open %s.\n", infile);
-        return 2;
+        printf("Could not open file.\n");
+        return 1;
     }
 
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    // Define a buffer to store a block of the forensic image and an integer to keep track of the image count
+    BYTE buffer[BLOCK_SIZE];
+    int imageCount = 0;
+
+    // Define a file pointer for the current image file and a character array for the filename
+    FILE *currentImage = NULL;
+    char filename[FILE_NAME_SIZE];
+
+    // Read 512-byte blocks from the file
+    while (fread(buffer, BLOCK_SIZE, 1, file))
     {
+        // Check if the start of the block is a JPEG signature
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // If a JPEG file is already open, close it
+            if (currentImage != NULL)
+            {
+                fclose(currentImage);
+            }
 
+            // Generate a new filename
+            sprintf(filename, "%03i.jpg", imageCount);
 
+            // Open a new JPEG file
+            currentImage = fopen(filename, "w");
+
+            // Increase the image count
+            imageCount++;
+        }
+
+        // If a JPEG file is open, write the block to it
+        if (currentImage != NULL)
+        {
+            fwrite(buffer, BLOCK_SIZE, 1, currentImage);
+        }
     }
 
-    // use a for loop to create as many output files of format .jpg as are found in the input file
-    // Open output file
-
-    char *outfile = argv[2];
-    FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
+    // Close the last JPEG file
+    if (currentImage != NULL)
     {
-        fclose(inptr);
-        printf("Could not create %s.\n", outfile);
-        return 3;
+        fclose(currentImage);
     }
 
+    // Close the forensic image file
+    fclose(file);
 
-
+    // Exit with a return code of 0 (success)
+    return 0;
 }
