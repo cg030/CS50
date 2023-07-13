a81e9ba1afd8e830a3d0b9a2fce6278398b912d9
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-13 08:14:20 +0000

    Thu, Jul 13, 2023, 10:14 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,20 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
+    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
     if (argc != 2)
     {
         printf("Improper usage; Only one command line argumment\n");
         return(1);
     }
 
+    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
     FILE *file = fopen(argv[1], "r");
 
-    
-
-
-    // Your program should accept exactly one command-line argument, the name of a forensic image from which to recover JPEGs.
-    // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
-
-    // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
 
 }
