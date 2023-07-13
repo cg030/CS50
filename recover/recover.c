6257269868f5b43e37b851f70103a2b131801d7d
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-13 08:05:56 +0000

    Thu, Jul 13, 2023, 10:05 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,5 +7,5 @@ int main(int argc, char *argv[])
     // If your program is not executed with exactly one command-line argument, it should remind the user of correct usage, and main should return 1.
 
     // If the forensic image cannot be opened for reading, your program should inform the user as much, and main should return 1.
-
+    
 }
