3994225eeb4a6c635fed88bc7ec0c9f5e6a4d0fd
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-20 12:21:55 +0000

    Thu, Jul 20, 2023, 2:21 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -64,9 +64,11 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-
+        n -> parents[0] = NULL;
+        n -> parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        
 
     }
 
