1a60bb36f1757cdcdd955401eb7c97b3503187cb
cg030 <101114890+cg030@users.noreply.github.com>
2023-07-20 12:17:34 +0000

    Thu, Jul 20, 2023, 2:17 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -55,8 +55,8 @@ person *create_family(int generations)
         n -> parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        n->alleles[0] = parent0->alleles[rand() % 2];
-        n->alleles[1] = parent1->alleles[rand() % 2];
+        n -> alleles[0] = parent0 -> alleles[rand() % 2];
+        n -> alleles[1] = parent1 -> alleles[rand() % 2];
 
     }
 
