487cb5d839414588d88eded4d1361a583e0ea54a
cg030 <101114890+cg030@users.noreply.github.com>
2023-08-27 18:38:41 +0000

    Sun, Aug 27, 2023, 8:38 PM +02:00

--- trie/trie.c
+++ trie/trie.c
@@ -117,7 +117,6 @@ bool check(char* word)
         // compare the index of given letter with index of letter in array and see if it's not NULL
         int index = tolower(word[i]) - 'a';
         
-        if
 
     }
     return false;
