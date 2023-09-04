254582af55e13db38c7a475bfef056f8a0a3b11e
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-04 14:25:52 +0000

    Mon, Sep 4, 2023, 4:25 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -152,6 +152,14 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            
+        }
+    }
     // TODO
     return false;
 }
