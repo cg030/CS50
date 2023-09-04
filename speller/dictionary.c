d2d983f28b378efa24d8f0df71047370bd6ab237
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-04 15:00:33 +0000

    Mon, Sep 4, 2023, 5:00 PM +02:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -52,8 +52,17 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // // TODO: Improve this hash function
+    // return toupper(word[0]) - 'A';
+
+    // djb2 hashing algorithm
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *word++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash % N; // Ensure the result fits into your table size
 }
 
 // Loads dictionary into memory, returning true if successful, else false
