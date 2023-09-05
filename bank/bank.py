8c53fd4909536289868b27372a661053efc54668
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 08:58:48 +0000

    Tue, Sep 5, 2023, 10:58 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
 greeting = input('Greeting: ')
 greeting = greeting.lower().strip()
-if greeting == 'hello':
+if len(greeting == 'hello' or len(greeting) == 1:
     print(f'0$')
 elif greeting[0] == 'h':
     print(f'20$')
