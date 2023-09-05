49a88578c213e57a8c2a640220339b59ed15a82f
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 08:51:20 +0000

    Tue, Sep 5, 2023, 10:51 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,7 +2,7 @@ greeting = input('Greeting: ')
 greeting = greeting.lower().strip()
 if greeting == 'hello':
     print(f'100$')
-elif greeting[0] == 'h':
+elif greeting[0] == 'h' and greeting:
     print(f'20$')
 else:
     print(f'0$')
