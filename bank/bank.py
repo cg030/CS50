80dede23bd9c786bd0f6819ce95372f07e85634c
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 08:32:27 +0000

    Tue, Sep 5, 2023, 10:32 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,7 @@ import cs50 as cs
 greeting = input('Greeting: ')
 if greeting == 'hello':
     print(f'100$')
-elif greeting[1] == h:
+elif greeting[0] == 'h':
     print(f'20$')
 else:
     print(f'0$')
