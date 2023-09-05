2539dfde08a581e7d8ae37349f30936c66da06b6
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 08:27:15 +0000

    Tue, Sep 5, 2023, 10:27 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,3 +2,9 @@ import cs50 as cs
 
 while TRUE:
     greeting = input('Greeting: ')
+    if greeting == 'hello':
+        print(f'100$')
+    elif greeting[1] == h:
+        print(f'20$')
+    else:
+        print(f'0$')
