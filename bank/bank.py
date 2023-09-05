bf230547f7a5294a6c744ec7009127e59f27299e
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 09:15:34 +0000

    Tue, Sep 5, 2023, 11:15 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -5,7 +5,7 @@ words = greeting.lower().strip().split()
 
 if words[0] == 'hello':
     print(f'0$')
-elif greeting[0] == 'h':
+elif words[0][0] == 'h':
     print(f'20$')
 else:
     print(f'100$')
