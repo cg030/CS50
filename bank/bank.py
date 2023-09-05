48446aabe8e05af125dca23cf91826143514131d
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-05 09:31:59 +0000

    Tue, Sep 5, 2023, 11:31 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -3,8 +3,8 @@ words = [word.strip(',.!?') for word in greeting.lower().split()]
 
 
 if words[0] == 'hello':
-    print(f'0$')
+    print(f'0$', end ='')
 elif words[0][0] == 'h':
-    print(f'20$')
+    print(f'20$', end ='')
 else:
-    print(f'100$')
+    print(f'100$', end ='')
