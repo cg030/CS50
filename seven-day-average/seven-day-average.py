f44a13a1d96cdece7dd858c1c00d31603d156c95
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 11:29:20 +0000

    Thu, Sep 7, 2023, 1:29 PM +02:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -39,9 +39,7 @@ def calculate(reader):
         # state_name : list of most 14 days of new cases
     }
      # create second dictionary
-     state_dict_single = {
-         
-     }
+     state_dict_single = state_dict_cumulative["]
 
 
 # TODO: Calculate and print out seven day average for given state
