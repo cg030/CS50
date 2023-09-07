6b4f161b3270fbff0dc0b89ab4792ca882219634
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 08:03:40 +0000

    Thu, Sep 7, 2023, 10:03 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -14,3 +14,6 @@ if len(argv) == 0:
 elif len(argv) == 2 and argv[0] == '-f' or argv[0] == '--font' and argv[1] == # valid font from available_fonts
     y = fi.figlet_format(input, font = argv[1])
     print(f'Output: {y}')
+else
+    print(f'Invalid command-line argumment')
+    return()
