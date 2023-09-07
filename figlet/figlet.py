58be7683cd41ce53a0e33c92e956fd1190379f61
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 08:23:04 +0000

    Thu, Sep 7, 2023, 10:23 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,8 +1,8 @@
-from pyfiglet import Figlet
+from pyfiglet import Figlet, figlet_format
 import random
 import sys
 
-user_input = input('Input:')  
+user_input = input('Input:')
 
 available_fonts = Figlet().getFonts()
 
@@ -10,11 +10,12 @@ random_font = random.choice(available_fonts)
 
 # Use sys.argv instead of argv
 if len(sys.argv) == 1:
-    x = Figlet().figlet_format(user_input, font=random_font)
+    x = figlet_format(user_input, font=random_font)
     print(f'Output: {x}')
 elif len(sys.argv) == 3 and (sys.argv[1] == '-f' or sys.argv[1] == '--font') and sys.argv[2] in available_fonts:
-    y = Figlet().figlet_format(user_input, font=sys.argv[2])
+    y = figlet_format(user_input, font=sys.argv[2])
     print(f'Output: {y}')
 else:
     print('Invalid command-line argument')
     sys.exit(1)
+
