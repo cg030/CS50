e05ae016a7daf2dd596905203b2a02b1673b83f1
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 08:45:05 +0000

    Thu, Sep 7, 2023, 10:45 AM +02:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,6 +1,9 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        if __init__ >= 0:
+            return 0
+        else:
+            return raiseValueError
 
     def __str__(self):
         ...
