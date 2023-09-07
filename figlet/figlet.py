f4c38cf4b74118a695680566224c7d28a433571d
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 08:02:05 +0000

    Thu, Sep 7, 2023, 10:02 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -10,7 +10,7 @@ random_font = random.choice(available fonts)
 
 if len(argv) == 0:
     x = fi.figlet_format(input, font = random_font)
-    print(f'{x}')
+    print(f'Output: {x}')
 elif len(argv) == 2 and argv[0] == '-f' or argv[0] == '--font' and argv[1] == # valid font from available_fonts
     y = fi.figlet_format(input, font = argv[1])
-    print(f'{y}')
+    print(f'Output: {y}')
