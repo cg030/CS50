ba24eaf134dadfb0491c67bcaa8c0da27b6de56c
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-07 07:51:24 +0000

    Thu, Sep 7, 2023, 9:51 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,12 @@
 import figlet as fi
+import random
 from sys import argv
 
 input = input(print(f'Input:'))
 
+available_fonts = fi.Figlet().getFonts()
+
+random_font = random.choice(available fonts)
+
 if len(argv) == 0:
-    
+
