508523ac438a343c842fa1ab71f706419882eb04
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 10:54:13 +0000

    Wed, Sep 13, 2023, 12:54 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,6 +3,6 @@ def main():
     number = input('Number: ')
 
 def luhn(number):
-    for i in range(number[len(number) - 1], 0, -1):
+    for i in range(number[len(number)], 0, -1):
         sum_product += i * 2
-        
+        sum_product
