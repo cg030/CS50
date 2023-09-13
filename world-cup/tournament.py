f657f8ae6a259a69878ba498e32617781aca5169
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 08:02:35 +0000

    Wed, Sep 13, 2023, 10:02 AM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -56,7 +56,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    
+    # first I have to simulate games; this is actually done by simulate_round
 
 
 if __name__ == "__main__":
