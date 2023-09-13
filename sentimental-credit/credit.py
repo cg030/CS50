cbce6bc0350473402eec5904bb2cc84fa0061c9a
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 11:32:22 +0000

    Wed, Sep 13, 2023, 1:32 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,3 +1,18 @@
+def main():
+    # get input from user
+    number = input('Number: ')
+
+    while not number.isnumeric():
+        number = input('Number: ')
+
+if (luhn(number) == True and card_type == 'AMEX'):
+    print(f'AMEX')
+elif (luhn(number) == True and card_type == 'MASTERCARD'):
+    print(f'MASTERCARD')
+elif (luhn(number) == True and card_type == 'VISA'):
+    print(f'VISA')
+else:
+    print(f'INVALID')
 def luhn(number):
     for i in range(number[len(number - 1)], 0, -2):
         sum_product += i * 2
@@ -23,22 +38,5 @@ def card_type(number):
         return('MasterCard')
     else:
         return('InvalidCard')
-
-def main():
-    # get input from user
-    number = input('Number: ')
-
-    while not number.isnumeric():
-        number = input('Number: ')
-
-if (luhn(number) == True and card_type == 'AMEX'):
-    print(f'AMEX')
-elif (luhn(number) == True and card_type == 'MASTERCARD'):
-    print(f'MASTERCARD')
-elif (luhn(number) == True and card_type == 'VISA'):
-    print(f'VISA')
-else:
-    print(f'INVALID')
-
 
 main()
