c85b0b28938280d00e609e3dc01cbadbe900fd05
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 10:42:45 +0000

    Wed, Sep 13, 2023, 12:42 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,7 @@
 def main():
     # get input from user
-    user_input = int(input('Number: '))
+    number = int(input('Number: '))
 
 def luhn(number):
     while(number > 0):
         digit = number % 10
-
-        if (count)
