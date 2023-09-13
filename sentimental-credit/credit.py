87e2b1c7308c38f6384353fee8f0ea92895cb42d
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 11:03:04 +0000

    Wed, Sep 13, 2023, 1:03 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,8 +2,20 @@ def main():
     # get input from user
     number = input('Number: ')
 
+
+
+
 def luhn(number):
     for i in range(number[len(number - 1)], 0, -2):
         sum_product += i * 2
     for j in range(number[len(number)], 0 , 1):
         sum += j
+
+    if ((sum_product + sum) % 10 == 0):
+        return True
+
+def card_type(number):
+    if (len(number) == 15):
+        return('AMEX')
+    elif (len(number) == 16):
+        return('MasterCard)
