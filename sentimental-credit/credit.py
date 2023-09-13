eaa332726f6b374112df676bd6a75f83eabf0293
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 13:09:22 +0000

    Wed, Sep 13, 2023, 3:09 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -20,13 +20,14 @@ def luhn(number):
     count = 0
     digit = int(number)
 
-    while (digit > 0):
-
+    while digit > 0:
         cur_digit = digit % 10
 
-        if (count % 2 != 0): # conditional for calculation according to digit order
+        if count % 2 != 0:  # conditional for calculation according to digit order
             product = cur_digit * 2
-            sum += product // 10 + product % 10 # if the product is a two-digit number this splits it into two digits; integer division on product by 10. Integer division in C rounds towards zero.
+            sum += (
+                product // 10 + product % 10
+            )  # if the product is a two-digit number this splits it into two digits; integer division on product by 10. Integer division in C rounds towards zero.
 
         else:
             sum += cur_digit
@@ -34,17 +35,16 @@ def luhn(number):
         count += 1
         digit = digit // 10 # remove last digit
 
-    return (sum % 10 == 0)
+    return sum % 10 == 0
 
 
 def card_type(number):
-    if (len(number) == 15 and (number[:2] == '34' or number[:2] == '37')):
-        return('AMEX')
-    elif ((len(number) == 13 or len(number) == 16) and number[0] == '4'):
-        return('VISA')
-    elif (len(number) == 16 and (number[:2] in ('51', '52', '53', '54', '55'))):
-        return('MASTERCARD')
+    if len(number) == 15 and (number[:2] == '34' or number[:2] == '37'):
+        return 'AMEX'
+    elif (len(number) == 13 or len(number) == 16) and number[0] == '4':
+        return 'VISA'
+    elif len(number) == 16 and (number[:2] in ('51', '52', '53', '54', '55')):
+        return 'MASTERCARD'
     else:
         return False
-
 main()
