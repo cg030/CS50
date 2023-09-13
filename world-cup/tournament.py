b492968025f070aebaa5e9e590482d7a3185011a
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-13 08:31:29 +0000

    Wed, Sep 13, 2023, 10:31 AM +02:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,6 +26,11 @@ def main():
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            counts[winner] += 1
+        else:
+            counts[winner] 
         counts['Name'] = simulate_tournament(teams)
         counts['Wins'] += 1
 
