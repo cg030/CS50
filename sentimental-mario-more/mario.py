e3e3ea964a6cf605d1ab511546d557980600effd
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 13:58:35 +0000

    Thu, Sep 14, 2023, 3:58 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -6,25 +6,25 @@ def main():
             break
 
     for i in range(height):
-        print('' * (height - 1) + '#' * i + '' + )
+        print('' * (height - 1) + '#' * i + '' + '#' * i +  '' * (height - 1))
 
-    # loop to print pyramid
-    for i in range(1, height, 1):
+    # # loop to print pyramid
+    # for i in range(1, height, 1):
 
-        # loop to first print spaces
-        for j in range(1, height - i, 1):
-            print(' ', end = '')
+    #     # loop to first print spaces
+    #     for j in range(1, height - i, 1):
+    #         print(' ', end = '')
 
-        # loop to print the number of hashes equivalent to the row; +1 row, +1 hash
-        for k in range(1, j <=i, 1):
-            print('#', end = '')
+    #     # loop to print the number of hashes equivalent to the row; +1 row, +1 hash
+    #     for k in range(1, j <=i, 1):
+    #         print('#', end = '')
 
-        print(' ', end = '')
+    #     print(' ', end = '')
 
-        # identical for loop as above
-        for j in range(1, j<=i, 1):
-            print('#', end = '')
+    #     # identical for loop as above
+    #     for j in range(1, j<=i, 1):
+    #         print('#', end = '')
 
-        print()
+    #     print()
 
 main()
