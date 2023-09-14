32c87f8367a6ef133bdeb4b51d1f189da0049c89
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 14:12:11 +0000

    Thu, Sep 14, 2023, 4:12 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,10 +1,14 @@
 def main():
     # get size input
     while True:
-        height = int(input('Height: '))
-        if 1 <= height <= 8:
-            break
-        elif 
+        try:
+            height = int(input('Height: '))
+            if 1 <= height <= 8:
+                break
+            else:
+                print(f'Enter a value between 1 and 8')
+        except ValueError:
+            print(f'Invalid Input')
 
     for i in range(1, height + 1 , 1):
         print(' ' * (height - i) + '#' * i + '  ' + '#' * i)
