39f84b3e6160e9485a15a28e10ed5e02eb1b6ddc
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 14:46:53 +0000

    Thu, Sep 14, 2023, 4:46 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,24 +1,34 @@
 def main():
 
+    # Get user input
     while True:
         try:
             user_input = input('Text: ')
-            if user_input = isalpha:
+            if user_input.isalpha():
                 break
             else:
                 print(f'Enter text')
         except ValueError:
             print(f'Invalid Input')
 
+    # calculate Index
+
+    L = count_letters(user_input) / count_words(user_input) * 100 # L is the average number of letters per 100 words in the text
+    S = count_sentences(user_input) / count_words(user_input) * 100 # S is the average number of sentences per 100 words in the text
+    score = 0.0588 * L - 0.296 * S - 15.8
+
+    # print score
+    
 
 def count_letters(user_input):
-    letter_count = sum(1 for char in user_input if char.isalpha())
-    return letter_count
+    letters = sum(1 for char in user_input if char.isalpha())
+    return letters
 
 
 def count_words(user_input):
-    word_count = len(user_input.split())
-    return word_count
+    words = len(user_input.split())
+    return words
 
 def count_sentences(user_input):
-    sentence_count = sum(1 for punc in user_input if )
+    sentences = sum(1 for punc in user_input if char in '.?!')
+    return sentences
