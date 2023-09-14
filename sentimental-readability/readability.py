3620a6ea419ce97d74a02ad589d3e29cc3231b06
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 14:34:13 +0000

    Thu, Sep 14, 2023, 4:34 PM +02:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,13 @@
+def main():
+
+    while True:
+        try:
+            user_input = input('Text: ')
+            if user_input = isalpha:
+                break
+            else:
+                print(f'Enter text')
+        except ValueError:
+            print(f'Invalid Input')
+
+    
