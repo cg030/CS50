b42108fd23ea8bbab07ac4226eb60b08d548a56b
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 13:47:34 +0000

    Thu, Sep 14, 2023, 3:47 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,5 +2,23 @@ def main():
     # get size input
     while True:
         height = int(input('Height: '))
-        if height < 1 or height > 8:
+        if 1 <= height <= 8:
             break
+    # loop to print pyramid
+    for i in range(1, height, 1):
+
+        # loop to first print spaces
+        for j in range(1, height - i, 1):
+            print(' ')
+
+        # loop to print the number of hashes equivalent to the row; +1 row, +1 hash
+        for k in range(1, j <=i, 1):
+            print('#')
+
+        print(' ')
+
+        # identical for loop as above
+        for j in range(1, j<=i, 1):
+            print('#')
+
+        print()
