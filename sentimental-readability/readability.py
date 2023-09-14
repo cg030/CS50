629b8ec369f44a9e9c36ab3069ed201f0af69853
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 14:38:36 +0000

    Thu, Sep 14, 2023, 4:38 PM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -10,4 +10,9 @@ def main():
         except ValueError:
             print(f'Invalid Input')
 
+def count_letters(user_input):
+    letter_count = sum(1 for char in user_input if char.isalpha())
+    return letter_count
+
+def count_words(user_input):
     
