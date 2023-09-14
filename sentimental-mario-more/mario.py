2259ee89a127d680984e51889bccd7bce3d7245a
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 14:09:08 +0000

    Thu, Sep 14, 2023, 4:09 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,6 +4,7 @@ def main():
         height = int(input('Height: '))
         if 1 <= height <= 8:
             break
+        elif 
 
     for i in range(1, height + 1 , 1):
         print(' ' * (height - i) + '#' * i + '  ' + '#' * i)
