e3914833168ff958472aebab315b804180cfdee4
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-14 13:37:20 +0000

    Thu, Sep 14, 2023, 3:37 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,4 +1,9 @@
 def main():
     # get size input
-    
+
     height = int(input('Height: '))
+
+    if height < 1 or height > 8:
+            break
+    else:
+          height = int(input('Height: '))
