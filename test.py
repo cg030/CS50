6623a08e288357e341f9ebc1ad6eccbb31b899ab
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-15 14:20:53 +0000

    Fri, Sep 15, 2023, 4:20 PM +02:00

---
+++ test.py
@@ -0,0 +1,14 @@
+test_dict = {
+    'Luffy' : 16,
+    'Zoro' : 20,
+    'Nami' : 18,
+    'Usopp' : 17,
+    'Sanji' : 19,
+    'Chopper' : 15,
+    'Robin' : 28,
+    'Franky' : 30,
+    'Brooke' : 80,
+    'Jimbei' : 40
+}
+
+print(test_dict)
