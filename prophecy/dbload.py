5edd449e0047365b1f2aaac2a3f6257f9f650389
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 11:06:00 +0000

    Sun, Sep 24, 2023, 1:06 PM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -16,12 +16,11 @@ with open("students.csv", "r") as file:
         student_name = row["student_name"]
         db.execute("INSERT INTO students(id,name) VALUES (?,?)", student_id,student_name)
 
-        # read data from csv file into the houses table in roster.db
+        # read data from csv file into the houses and assignments table in roster.db
         house_name = row["house"]
         house_head = row["head"]
         n = 1
         house_dict[row["house"]] = n
-
 
         if row["house"] == db.execute("SELECT house FROM houses"):
             # if the house alread exists in the houses table then you don't have to update the houses table
@@ -32,15 +31,4 @@ with open("students.csv", "r") as file:
             db.execute("INSERT INTO assignment(student_id, house_id) VALUES (?,?)", student_id, house_dict[row["house"]])
             n += 1
 
-
-
-        # if the house in the csv file already exists in the database skip row otherwise insert house into table
-
-        # if the value in house column in the csv file is equal to a house name in the house_dict, insert the corresponding id of the house in the dictionary into the assignments table
-        if row["house"] == house_dict("house_name"):
-            temp_house_id = row["house_name"]
-            db.execute("INSERT INTO assignment (student_id, house_id) VALUES (?,?)", student_id, temp_house_id)
-        # if the name doesn't yet exist in the dictionary add the house and id to the dictionary and then insert the id into the assignment table
-        else:
-            house_dict[row["house"]] = n
-            n += 1
+            
