18701b657bddd6bd77bab240ab034ac3b1c521ad
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 09:12:44 +0000

    Sun, Sep 24, 2023, 11:12 AM +02:00

---
+++ prophecy/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE houses {
+    id INTEGER,
+    house TEXT,
+    head TEXT,
+    PRIMARY KEY(house)
+}
+
+CREATE TABLE assignment {
+    student_id INTEGER,
+    house_id,
+    FOREIGN KEY(student_id)
+    FOREIGN KEY(house_id)
+}
+
