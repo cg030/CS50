e34efab904869015e5e2fd06700c9e99a7bf0403
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 09:44:02 +0000

    Sun, Sep 24, 2023, 11:44 AM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -2,15 +2,15 @@ from cs50 import SQL
 
 import csv
 
+# open database
+db = SQL("sqlite:///roster.db")
+
+db.execute("")
+
 with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         id = row["id"]
         name = row["student_name"]
         db.execute("INSERT INTO students(id,name) VALUES (?,?)", id,name)
-
-    
-
-# open database
-db = SQL("sqlite:///roster.db")
 
