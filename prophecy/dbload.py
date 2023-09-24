49b1a5ebb2e44cccd9d5a572875c34a4e9ba6f56
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 10:11:59 +0000

    Sun, Sep 24, 2023, 12:11 PM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -21,6 +21,7 @@ with open("students.csv", "r") as file:
         house_name = row["house"]
         house_head = row["head"]
         if row["house"] == db.execute("SELECT house FROM houses"):
+            
             next row
         else:
             db.execute("INSERT INTO houses(id, houses, head) VALUES (?,?,?)", house_id, house_name, house_head)
