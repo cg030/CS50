65931711e05727f2e80e7d0d8fe1cc5d1fb16a0c
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 09:56:30 +0000

    Sun, Sep 24, 2023, 11:56 AM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -10,16 +10,18 @@ with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
     # read data from csv file into the students table in the roster.db
     for row in reader:
-        id = row["id"]
-        name = row["student_name"]
-        db.execute("INSERT INTO students(id,name) VALUES (?,?)", id,name)
+        student_id = row["id"]
+        student_name = row["student_name"]
+        db.execute("INSERT INTO students(id,name) VALUES (?,?)", student_id,student_name)
 
     # read data from the csv file into the houses table in roster.db
     for row in reader:
-        # if the house in the csv file already exists in the database skip row
+        # if the house in the csv file already exists in the database skip row otherwise insert house into table
+        house_id = 1
+        house_name = row["house"]
+        house_head = row["head"]
         if row["house"] == db.execute("SELECT house FROM houses"):
             next row
         else:
-            
-
-
+            db.execute("INSERT INTO houses(id, houses, head) VALUES (?,?,?)", house_id, house_name, house_head)
+            house_id += 1
