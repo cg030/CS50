cd24b83641f66357fb24c1681b1d34cfc5ba2e00
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 11:59:08 +0000

    Sun, Sep 24, 2023, 1:59 PM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -1,9 +1,8 @@
+from cs50 import SQL
 import csv
-import sqlite3
 
-# Connect to the SQLite3 database
-conn = sqlite3.connect('roster.db')
-cursor = conn.cursor()
+# Set up a database connection
+db = SQL('sqlite:///roster.db')
 
 # Open the students.csv file
 with open('students.csv', 'r') as file:
@@ -12,22 +11,15 @@ with open('students.csv', 'r') as file:
         # Extract student_name, house, and head from each row in the CSV
         student_name, house, head = row
 
-        # Insert the student_name into the students table
-        cursor.execute('INSERT INTO students (student_name) VALUES (?)', (student_name,))
-        student_id = cursor.lastrowid  # Get the id of the last inserted row
+        # Insert the student_name into the students table and get the student_id
+        student_id = db.execute('INSERT INTO students (student_name) VALUES (:student_name)', student_name=student_name)
 
         # Check if the house already exists in the houses table, if not, insert it
-        cursor.execute('SELECT id FROM houses WHERE house_name = ?', (house,))
-        house_id = cursor.fetchone()
+        house_id = db.execute('SELECT id FROM houses WHERE house_name = :house', house=house)
         if not house_id:
-            cursor.execute('INSERT INTO houses (house_name, head) VALUES (?, ?)', (house, head))
-            house_id = cursor.lastrowid  # Get the id of the last inserted row
+            house_id = db.execute('INSERT INTO houses (house_name, head) VALUES (:house, :head)', house=house, head=head)
         else:
-            house_id = house_id[0]
+            house_id = house_id[0]['id']
 
         # Insert the student_id and house_id into the house_assignments table
-        cursor.execute('INSERT INTO house_assignments (student_id, house_id) VALUES (?, ?)', (student_id, house_id))
-
-# Commit the transactions and close the connection
-conn.commit()
-conn.close()
+        db.execute('INSERT INTO house_assignments (student_id, house_id) VALUES (:student_id, :house_id)', student_id=student_id, house_id=house_id)
