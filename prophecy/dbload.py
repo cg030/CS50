2adb736d6be4e05dddb69d0012b3ff4c983be529
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-24 09:32:42 +0000

    Sun, Sep 24, 2023, 11:32 AM +02:00

--- prophecy/dbload.py
+++ prophecy/dbload.py
@@ -2,6 +2,11 @@ from cs50 import SQL
 
 import csv
 
+with open("students.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        
+
 # open database
 db = SQL("sqlite:///roster.db")
 
