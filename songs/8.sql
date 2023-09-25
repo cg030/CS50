ec45480964250a5ba5ebf8b959bbbf274d132a92
cg030 <101114890+cg030@users.noreply.github.com>
2023-09-25 09:36:59 +0000

    Mon, Sep 25, 2023, 11:36 AM +02:00

--- songs/8.sql
+++ songs/8.sql
@@ -2,7 +2,6 @@
 -- Songs that feature other artists will include “feat.” in the name of the song.
 -- Your query should output a table with a single column for the name of each song.
 
-SELECT s.name
-FROM songs s
-JOIN artists a ON s.artist_id = a.id
-WHERE a.name LIKE '%feat.%';
+SELECT name
+FROM songs
+WHERE name LIKE '%feat.%';
