d9b4deb570c6957ef699bba2456124be0841155d
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-04 08:29:18 +0000

    Wed, Oct 4, 2023, 10:29 AM +02:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -118,7 +118,7 @@ AND f.hour = (SELECT MIN(f2.hour) FROM flights f2 WHERE f2.year = 2021 AND f2.mo
 -- | 8496433585      |
 -- +-----------------+
 
-SELECT p.id, p.passport_number, b.account_number, p.phone_number, p.license_plate
+SELECT p.name, p.id, p.passport_number, b.account_number, p.phone_number, p.license_plate
 FROM people p
 JOIN passengers pa ON p.passport_number = pa.passport_number
 JOIN bank_accounts b ON p.id = b.person_id
