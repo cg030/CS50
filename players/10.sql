71d5a279d71938e2c0a3357fc6388a091f3ff83e
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-05 17:24:56 +0000

    Thu, Oct 5, 2023, 7:24 PM +02:00

--- players/10.sql
+++ players/10.sql
@@ -1,4 +1,4 @@
-SELECT first_name AS 'First Name', last_name AS 'Last Name'
+SELECT first_name AS 'First Name', last_name AS 'Last Name', debut
 FROM players
 WHERE birth_country = 'Germany'
 ORDER BY debut DESC;
