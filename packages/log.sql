1cc28ec0a72cadd96346173945c1c3eae17e3e34
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-16 14:19:35 +0000

    Mon, Oct 16, 2023, 4:19 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -17,8 +17,14 @@ JOIN addresses a ON p.to_address_id = a.id
 WHERE p.contents LIKE '%duck%'
 AND p.from_address_id IS NULL;
 
+SELECT a.type, p.contents
+FROM packages p
+JOIN addresses a ON p.to_address_id = a.id
+WHERE p.contents LIKE '%duck%'
+AND p.from_address_id IS NULL;
+
+
 -- *** The Forgotten Gift ***
--- text
 
 SELECT p.contents , d.name
 FROM packages p
@@ -29,19 +35,3 @@ JOIN drivers d ON s.driver_id = d.id
 WHERE a.address = "109 Tileston Street"
 AND a2.address = "728 Maple Place"
 ORDER BY s.timestamp DESC;
-
-SELECT
-    p.contents AS package_contents,
-    d.name AS driver_name
-FROM
-    packages p
-    LEFT JOIN scans s ON p.id = s.package_id
-    LEFT JOIN drivers d ON s.driver_id = d.id
-    INNER JOIN addresses a_from ON p.from_address_id = a_from.id
-    INNER JOIN addresses a_to ON p.to_address_id = a_to.id
-WHERE
-    a_from.address = '109 Tileston Street'
-    AND a_to.address = '728 Maple Place'
-ORDER BY
-    s.timestamp DESC
-LIMIT 1;
