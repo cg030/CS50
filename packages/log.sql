2301786b7466138e1e297da9372746b1d7bc18f5
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-16 14:15:53 +0000

    Mon, Oct 16, 2023, 4:15 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -10,6 +10,7 @@ WHERE a.address = "900 Somerville Avenue"
 AND a2.address LIKE "2%Finn%gan% Street";
 
 -- *** The Devious Delivery ***
+-- text
 SELECT a.type, p.contents
 FROM packages p
 JOIN addresses a ON p.to_address_id = a.id
@@ -17,6 +18,7 @@ WHERE p.contents LIKE '%duck%'
 AND p.from_address_id IS NULL;
 
 -- *** The Forgotten Gift ***
+-- text
 
 SELECT p.contents , d.name
 FROM packages p
@@ -26,14 +28,20 @@ JOIN scans s ON p.id = s.package_id
 JOIN drivers d ON s.driver_id = d.id
 WHERE a.address = "109 Tileston Street"
 AND a2.address = "728 Maple Place"
-AND s.action = "Drop";
+ORDER BY s.timestamp DESC;
 
-SELECT p.contents , d.name
-FROM packages p
-JOIN addresses a ON p.from_address_id = a.id
-JOIN addresses a2 ON p.to_address_id = a2.id
-LEFT JOIN scans s ON p.id = s.package_id
-LEFT JOIN drivers d ON s.driver_id = d.id
-WHERE a.address = "109 Tileston Street"
-AND a2.address = "728 Maple Place"
-AND s.action = "Drop";
+SELECT
+    p.contents AS package_contents,
+    d.name AS driver_name
+FROM
+    packages p
+    LEFT JOIN scans s ON p.id = s.package_id
+    LEFT JOIN drivers d ON s.driver_id = d.id
+    INNER JOIN addresses a_from ON p.from_address_id = a_from.id
+    INNER JOIN addresses a_to ON p.to_address_id = a_to.id
+WHERE
+    a_from.address = '109 Tileston Street'
+    AND a_to.address = '728 Maple Place'
+ORDER BY
+    s.timestamp DESC
+LIMIT 1;
