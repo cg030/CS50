b32547b230e87233b0ca70024faf070b4e86c129
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-16 14:38:20 +0000

    Mon, Oct 16, 2023, 4:38 PM +02:00

--- packages/log.sql
+++ packages/log.sql
@@ -10,13 +10,12 @@ WHERE a.address = "900 Somerville Avenue"
 AND a2.address LIKE "2%Finn%gan% Street";
 
 -- *** The Devious Delivery ***
-SELECT a_to.type, p.contents
+SELECT a.type, p.contents
 FROM packages p
-JOIN addresses a_to ON p.to_address_id = a_to.id
+JOIN addresses a ON p.to_address_id = a.id
 LEFT JOIN addresses a_from ON p.from_address_id = a_from.id
 WHERE p.contents LIKE '%duck%'
-AND a_from.address IS NULL;
-
+AND (p.from_address_id IS NULL OR a_from.address IS NULL);
 
 -- *** The Forgotten Gift ***
 
