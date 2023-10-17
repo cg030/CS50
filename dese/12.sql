dccb3fdaa0baad88e9479f25f701e23368d3cdbf
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 08:26:43 +0000

    Tue, Oct 17, 2023, 10:26 AM +02:00

--- dese/12.sql
+++ dese/12.sql
@@ -2,5 +2,6 @@ SELECT d.name, e.per_pupil_expenditure, se.exemplary
 FROM districts d
 JOIN expenditures e ON d.id = e.district_id
 JOIN staff_evaluations se ON d.id = se.district_id
-WHERE e.per_pupil_expenditures >= (SELECT AVG(per_pupil_expenditures) FROM expenditures)
-AND se.exemplary >= (SELECT AVG(exemplary) FROM staff_evaluations);
+WHERE e.per_pupil_expenditure >= (SELECT AVG(per_pupil_expenditure) FROM expenditures)
+AND se.exemplary >= (SELECT AVG(exemplary) FROM staff_evaluations)
+ORDER BY se.exemplary DESC, e.per_pupil_expenditure DESC;
