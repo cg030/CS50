e60f632bd2b0caccf55f353ddf4285902b6fdba6
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 09:10:25 +0000

    Tue, Oct 17, 2023, 11:10 AM +02:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -6,3 +6,8 @@ WHERE p.first_name = 'Satchel'
 AND p.last_name = 'Paige';
 
 
+SELECT DISTINCT t.name
+FROM players p
+JOIN performances pf ON p.id = pf.player_id
+JOIN teams t ON pf.team_id = t.id
+WHERE p.first_name = 'Satchel' AND p.last_name = 'Paige';
