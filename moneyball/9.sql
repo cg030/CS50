038854da6d5fc75e8832ed98aa30fed32c9158d8
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 10:03:10 +0000

    Tue, Oct 17, 2023, 12:03 PM +02:00

--- moneyball/9.sql
+++ moneyball/9.sql
@@ -1,7 +1,7 @@
 SELECT ROUND(AVG(s.salary),2) AS 'average salary', t.name
 FROM salaries s
 JOIN teams t ON s.team_id = t.id
+WHERE s.year = 2001
 GROUP BY s.team_id
-HAVING s.year = 2001
-ORDER BY ROUND(AVG(s.salary),2) ASC
+ORDER BY 'average salary' ASC
 LIMIT 5;
