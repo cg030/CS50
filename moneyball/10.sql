5521535a65d51ceccbb44a7f9a1b5514d6a039f2
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 10:09:49 +0000

    Tue, Oct 17, 2023, 12:09 PM +02:00

--- moneyball/10.sql
+++ moneyball/10.sql
@@ -1,2 +1,5 @@
 SELECT p.first_name, p.last_name, s.salary, per.HR, per.year
-ORDER BY p.id ASC, per.year DESC, per.HR DESC, s.salary DESC
+FROM players p
+JOIN salaries s ON p.id = s.player_id
+JOIN performances per ON p.id = per.player_id
+ORDER BY p.id;
