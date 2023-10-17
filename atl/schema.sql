4d6e3a36b807605c6baa7e1b0a17f7c96fed02da
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 16:50:18 +0000

    Tue, Oct 17, 2023, 6:50 PM +02:00

--- atl/schema.sql
+++ atl/schema.sql
@@ -1,21 +1,28 @@
-CREATE TABLE "passengers" (
+CREATE TABLE passengers (
     id,
     first_name,
-    "last_name",
-    "age",
+    last_name,
+    age,
     PRIMARY KEY(id)
 )
 
-CREATE TABLE "check_ins" (
-    "date_time" DATE,
-    FOREIGN KEY("passenger_id") REFERENCES passengers("id")
+CREATE TABLE check_ins (
+    date_time DATE,
+    FOREIGN KEY(passenger_id) REFERENCES passengers(id),
+    FOREIGN KEY(flight_id) REFERENCES flights(id)
 )
 
 CREATE TABLE "airlines" (
-
+    id,
+    name,
+    concourse CHECK(A,B,C,D,E,F,T)
+    PRIMARY KEY(id)
 )
 
 CREATE TABLE "flights" (
-
+    id,
+    flight_number,
+    
+    FOREIGN KEY(airline_id) REFERENCES airlines(id)
 )
 
