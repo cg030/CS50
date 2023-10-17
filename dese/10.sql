9418dc4a2afe907a397f6b040b69035aaccae5c5
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 08:06:22 +0000

    Tue, Oct 17, 2023, 10:06 AM +02:00

--- dese/10.sql
+++ dese/10.sql
@@ -1,4 +1,5 @@
-SELECT * FROM expenditures;
-
--- SELECT d.name , e.per_pupil_expenditure
-
+SELECT d.name , e.per_pupil_expenditure
+FROM districts d
+JOIN expenditures e ON d.id = e.district_id
+ORDER BY e.per_pupil_expenditure DESC
+LIMIT 10;
