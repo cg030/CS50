2e6042314f5344f6b8e267bde7be4830cecea5e0
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 08:52:56 +0000

    Tue, Oct 17, 2023, 10:52 AM +02:00

--- moneyball/3.sql
+++ moneyball/3.sql
@@ -1,1 +1,7 @@
-SELECT 
+SELECT per.year, per.HR
+FROM performances per
+JOIN players p ON per.player_id = p.id
+WHERE p.first_name = 'Ken'
+AND p.last_name = 'Griffey'
+AND p.birth_year = 1969
+ORDER BY per.year DESC;
