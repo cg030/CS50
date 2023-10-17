e73cf69972364cf3606af3b6ce130f7e44a0e5dc
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 09:11:46 +0000

    Tue, Oct 17, 2023, 11:11 AM +02:00

--- moneyball/5.sql
+++ moneyball/5.sql
@@ -1,11 +1,3 @@
-SELECT t.name
-FROM teams t
-JOIN salaries s ON t.id = s.team_id
-JOIN players p ON s.player_id = p.id
-WHERE p.first_name = 'Satchel'
-AND p.last_name = 'Paige';
-
-
 SELECT DISTINCT t.name
 FROM players p
 JOIN performances pf ON p.id = pf.player_id
