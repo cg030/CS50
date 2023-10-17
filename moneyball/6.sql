b8470d09df2c1ab60bc88fbe67038ec979503d0c
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 09:24:14 +0000

    Tue, Oct 17, 2023, 11:24 AM +02:00

--- moneyball/6.sql
+++ moneyball/6.sql
@@ -2,6 +2,6 @@ SELECT t.name , per.H AS 'total hits'
 FROM teams t
 JOIN performances per ON t.id = per.team_id
 WHERE t.year = 2001
-GROUP BY t.name
+-- GROUP BY t.name
 ORDER BY per.H DESC
 LIMIT 5;
