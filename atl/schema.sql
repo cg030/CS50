7fee505a270fb77b26d9dada794e16cb1265e544
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 16:44:16 +0000

    Tue, Oct 17, 2023, 6:44 PM +02:00

---
+++ atl/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE "passengers" (
+    "id",
+    "first_name",
+    "last_name",
+    "age",
+    PRIMARY KEY("id")
+)
+
+CREATE TABLE "check_ins" (
+    "date_time" DATE,
+    FOREIGN KEY("passenger_id") REFERENCES passengers("id")
+)
+
+CREATE TABLE "airlines" (
+
+)
+
+CREATE TABLE "flights" (
+
+)
+
