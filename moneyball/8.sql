37d4a03b95ac819ddaf785d6c291fcaf4b3c4dda
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 09:53:39 +0000

    Tue, Oct 17, 2023, 11:53 AM +02:00

--- moneyball/8.sql
+++ moneyball/8.sql
@@ -1,9 +1,9 @@
-SELECT s.salary, p.first_name , p.last_name
+SELECT s.salary, p.first_name , p.last_name, s.year
 FROM salaries s
 JOIN players p ON s.player_id = p.id
 JOIN performances per ON p.id = per.player_id
 WHERE per.year = 2001
-AND s.year = 2001
+-- AND s.year = 2001
 AND per.HR = (SELECT MAX(HR) FROM performances WHERE year = 2001);
 
 
