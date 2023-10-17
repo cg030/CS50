55dfc75b38bcb98187b60544de830462ed449309
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-17 08:46:57 +0000

    Tue, Oct 17, 2023, 10:46 AM +02:00

--- moneyball/2.sql
+++ moneyball/2.sql
@@ -1,5 +1,5 @@
-SELECT s.year, s.salary
+SELECT s.year, s.salary, p.first_name, p.last_name
 FROM salaries s
 JOIN players p ON s.player_id = p.id
-WHERE p.first_name LIKE '%Cal%'
-AND p.last_name LIKE '%Ripken Jr.%';
+WHERE p.first_name LIKE '%Calvin%';
+-- AND p.last_name LIKE '%Ripken Jr.%'
