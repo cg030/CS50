c47aaca7c3945925f583a15dcd1264908b6150e2
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 09:39:12 +0000

    Fri, Oct 20, 2023, 11:39 AM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,11 +1,13 @@
--- First change the password to oops in the users table, which will create a log
-UPDATE users
-SET password = '982c0381c279d139fd221fce974916e7'
-WHERE id = 1;
+-- -- First change the password to oops in the users table, which will create a log
+-- UPDATE users
+-- SET password = '982c0381c279d139fd221fce974916e7'
+-- WHERE id = 1;
 
--- update this log to appear as if the password was change to emily's password
--- set the password of the admin to that of 'emily33' which will trigger the change in the user_log table
-UPDATE user_logs
-SET password = (SELECT password FROM users WHERE username = 'emily33'), new_username = 'emily33'
+-- -- update this log to appear as if the password was change to emily's password
+-- -- set the password of the admin to that of 'emily33' which will trigger the change in the user_log table
+-- UPDATE user_logs
+-- SET password = (SELECT password FROM users WHERE username = 'emily33')
 WHERE old_username = admin;
 
+SELECT * FROM users WHERE id = 1;
+SELECT * FROM user_logs WHERE old_username = 'admin' OR old_username = 'emily33';
