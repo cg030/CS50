8957e27e883004431294c95b6d1aea428f44326f
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 09:43:28 +0000

    Fri, Oct 20, 2023, 11:43 AM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -7,7 +7,7 @@ WHERE id = 1;
 -- set the password of the admin to that of 'emily33' which will trigger the change in the user_log table
 UPDATE user_logs
 SET new_password = (SELECT new_password FROM user_logs WHERE new_username = 'emily33')
-WHERE old_username = admin;
+WHERE old_username = 'admin';
 
-SELECT * FROM users WHERE id = 1;
-SELECT * FROM user_logs WHERE old_username = 'admin' OR new_username = 'emily33';
+-- SELECT * FROM users WHERE id = 1;
+-- SELECT * FROM user_logs WHERE old_username = 'admin' OR new_username = 'emily33';
