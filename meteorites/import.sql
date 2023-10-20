c58e5ff5c11d936a8859c43fe0b5ddb6cf59bf35
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 10:17:13 +0000

    Fri, Oct 20, 2023, 12:17 PM +02:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,16 +1,17 @@
-DROP TABLE meteorites;
+-- DROP TABLE meteorites;
 
-CREATE TABLE meteorites (
-    name TEXT,
-    id INTEGER,
-    nametype TEXT,
-    class TEXT,
-    mass REAL,
-    discovery TEXT,
-    year INTEGER,
-    lat REAL,
-    long REAL
-);
+-- CREATE TABLE meteorites (
+--     name TEXT,
+--     id INTEGER,
+--     nametype TEXT,
+--     class TEXT,
+--     mass REAL,
+--     discovery TEXT,
+--     year INTEGER,
+--     lat REAL,
+--     long REAL
+-- );
 
 -- Any empty values in meteorites.csv are represented by NULL in the meteorites table.
 
+SELECT DISTINCT mass FROM meteorites;
