d52da546d31d370cde8944f34000e104e1a2d3fe
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 09:10:08 +0000

    Fri, Oct 20, 2023, 11:10 AM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -6,4 +6,4 @@ WHERE id = 1;
 
 -- If I execute this the Trigger will be executed.
 -- remove the trigger
--- 
+-- in user log table change the password of admin to that of emily33's. Do this by changing it in the users table, which will activate the trigger and log 
