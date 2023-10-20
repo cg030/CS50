d2d030720195866fb986f936e04bccc952d362ba
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 11:28:08 +0000

    Fri, Oct 20, 2023, 1:28 PM +02:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,5 +1,5 @@
 DROP TABLE meteorites;
-
+DROP TABLE meteorites;
 
 CREATE TABLE meteorites_import (
     name TEXT,
@@ -24,55 +24,55 @@ CREATE TABLE meteorites (
     long REAL
 );
 
--- Any empty values in meteorites.csv are represented by NULL in the meteorites table.
-SELECT * FROM meteorites_import WHERE mass IS NULL;
-SELECT * FROM meteorites_import WHERE lat IS NULL;
-SELECT * FROM meteorites_import WHERE long IS NULL;
-SELECT * FROM meteorites_import;
--- .import automatically converts empty values to NULL
+-- -- Any empty values in meteorites.csv are represented by NULL in the meteorites table.
+-- SELECT * FROM meteorites_import WHERE mass IS NULL;
+-- SELECT * FROM meteorites_import WHERE lat IS NULL;
+-- SELECT * FROM meteorites_import WHERE long IS NULL;
+-- SELECT * FROM meteorites_import;
+-- -- .import automatically converts empty values to NULL
 
 
--- Update decimal values
-UPDATE meteorites_import
-SET mass = ROUND(mass, 2),
-    lat = ROUND(lat, 2),
-    long = ROUND(long, 2);
+-- -- Update decimal values
+-- UPDATE meteorites_import
+-- SET mass = ROUND(mass, 2),
+--     lat = ROUND(lat, 2),
+--     long = ROUND(long, 2);
 
 
--- Delete rows
-DELETE FROM meteorites_import
-WHERE nametype = 'Relict';
+-- -- Delete rows
+-- DELETE FROM meteorites_import
+-- WHERE nametype = 'Relict';
 
--- DROP columns; no direct way in sqlite3 so you have to create temporary tables
-INSERT INTO meteorites (id, name, class, mass, discovery, year, lat, long)
-SELECT id, name, class, mass, discovery, year, lat, long
-FROM meteorites_import;
+-- -- DROP columns; no direct way in sqlite3 so you have to create temporary tables
+-- INSERT INTO meteorites (id, name, class, mass, discovery, year, lat, long)
+-- SELECT id, name, class, mass, discovery, year, lat, long
+-- FROM meteorites_import;
 
-DROP TABLE meteorites_import;
+-- DROP TABLE meteorites_import;
 
 
--- sort rows
--- backup the data in a temporary table
-CREATE TEMP TABLE temp_table AS
-SELECT * FROM meteorites;
+-- -- sort rows
+-- -- backup the data in a temporary table
+-- CREATE TEMP TABLE temp_table AS
+-- SELECT * FROM meteorites;
 
--- drop original table
-DROP TABLE meteorites;
+-- -- drop original table
+-- DROP TABLE meteorites;
 
--- recreat the original table with the sorted temporary table
-CREATE TABLE meteorites AS
-SELECT
-    ROW_NUMBER() OVER(ORDER BY year ASC, name ASC) AS id,
-    name,
-    class,
-    mass,
-    discovery,
-    year,
-    lat,
-    long
-FROM temp_table;
+-- -- recreat the original table with the sorted temporary table
+-- CREATE TABLE meteorites AS
+-- SELECT
+--     ROW_NUMBER() OVER(ORDER BY year ASC, name ASC) AS id,
+--     name,
+--     class,
+--     mass,
+--     discovery,
+--     year,
+--     lat,
+--     long
+-- FROM temp_table;
 
--- drop temporary table
-DROP TABLE temp_table;
+-- -- drop temporary table
+-- DROP TABLE temp_table;
 
 
