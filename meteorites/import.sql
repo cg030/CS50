c5b7b7db8a6fa3a7d63172e2ea4f6d6a68f630ff
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 10:28:52 +0000

    Fri, Oct 20, 2023, 12:28 PM +02:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -15,7 +15,12 @@
 -- Any empty values in meteorites.csv are represented by NULL in the meteorites table.
 
 SELECT * FROM meteorites WHERE mass IS NULL;
-
 SELECT * FROM meteorites WHERE lat IS NULL;
-
 SELECT * FROM meteorites WHERE long IS NULL;
+
+-- .import automatically converts empty values to NULL
+
+UPDATE meteorites
+SET mass = ROUND(mass, 2),
+    lat = ROUND(lat, 2),
+    long = ROUND(long, 2)
