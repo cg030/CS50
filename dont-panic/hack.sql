9b13d6de2c4d353cf6067e13a4209a05e7fec6e4
cg030 <101114890+cg030@users.noreply.github.com>
2023-10-20 09:16:13 +0000

    Fri, Oct 20, 2023, 11:16 AM +02:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -6,4 +6,11 @@ WHERE id = 1;
 
 -- If I execute this the Trigger will be executed.
 -- remove the trigger
--- in user log table change the password of admin to that of emily33's. Do this by changing it in the users table, which will activate the trigger and log 
+-- in user log table change the password of admin to that of emily33's. Do this by changing it in the users table, which will activate the trigger and log.
+-- do this in reverse order
+
+-- set the password of the admin to that of 'emily33' which will trigger the change in the user_log table
+UPDATE users
+SET password = (SELECT password FROM users WHERE username = 'emily33')
+WHERE id = 1;
+
