141317932c32e9aab27ee169257ef081bd1fdf96
cg030 <101114890+cg030@users.noreply.github.com>
2023-11-30 17:19:23 +0000

    Thu, Nov 30, 2023, 6:19 PM +01:00

--- private/private.sql
+++ private/private.sql
@@ -1,4 +1,46 @@
 CREATE VIEW message AS
 SELECT SUBSTR(sentence, 98, 4)
 FROM sentences
-WHERE id = 14;
+WHERE id = 14
+
+UNION
+
+SELECT SUBSTR(sentence, 3, 5)
+FROM sentences
+WHERE id = 114
+
+UNION
+
+SELECT SUBSTR(sentence, 72, 9)
+FROM sentences
+WHERE id = 618
+
+UNION
+
+SELECT SUBSTR(sentence, 7, 3)
+FROM sentences
+WHERE id = 630
+
+UNION
+
+SELECT SUBSTR(sentence, 12, 5)
+FROM sentences
+WHERE id = 932
+
+UNION
+
+SELECT SUBSTR(sentence, 50, 7)
+FROM sentences
+WHERE id = 2230
+
+UNION
+
+SELECT SUBSTR(sentence, 44, 10)
+FROM sentences
+WHERE id = 2346
+
+UNION
+
+SELECT SUBSTR(sentence, 14, 10)
+FROM sentences
+WHERE id = 3041;
