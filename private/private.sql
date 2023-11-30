c57ce9848848300314b9dcba8ceff3a9b281d6d6
cg030 <101114890+cg030@users.noreply.github.com>
2023-11-30 17:42:22 +0000

    Thu, Nov 30, 2023, 6:42 PM +01:00

--- private/private.sql
+++ private/private.sql
@@ -5,43 +5,43 @@ SELECT SUBSTR(sentence, 98, 4) AS phrase
 FROM sentences
 WHERE id = 14
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 3, 5)
 FROM sentences
 WHERE id = 114
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 72, 9)
 FROM sentences
 WHERE id = 618
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 7, 3)
 FROM sentences
 WHERE id = 630
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 12, 5)
 FROM sentences
 WHERE id = 932
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 50, 7)
 FROM sentences
 WHERE id = 2230
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 44, 10)
 FROM sentences
 WHERE id = 2346
 
-UNION
+UNION ALL
 
 SELECT SUBSTR(sentence, 14, 5)
 FROM sentences
