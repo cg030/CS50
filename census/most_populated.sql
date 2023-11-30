7d357aa3edba29debad5b24e707721069dc36d81
cg030 <101114890+cg030@users.noreply.github.com>
2023-11-30 16:23:01 +0000

    Thu, Nov 30, 2023, 5:23 PM +01:00

--- census/most_populated.sql
+++ census/most_populated.sql
@@ -1,2 +1,15 @@
+DROP VIEW most_populated;
+
 CREATE VIEW most_populated AS
-SELECT 
+SELECT
+    district,
+    SUM(families) AS families,
+    SUM(households) AS households,
+    SUM(population) AS population,
+    SUM(male),
+    SUM(female)
+FROM census
+GROUP BY district
+ORDER BY SUM(population) DESC;
+
+SELECT * FROM by_district;
