dca951aab162f9f0bed3e8c28c7da3d46007d851
cg030 <101114890+cg030@users.noreply.github.com>
2023-11-30 16:18:12 +0000

    Thu, Nov 30, 2023, 5:18 PM +01:00

---
+++ census/by_district.sql
@@ -0,0 +1,14 @@
+DROP VIEW by_district;
+
+CREATE VIEW by_district AS
+SELECT
+    district,
+    SUM(families) AS families,
+    SUM(households) AS households,
+    SUM(population) AS population,
+    SUM(male),
+    SUM(female)
+FROM census
+GROUP BY district;
+
+SELECT * FROM by_district;
--- census/total.sql
+++ census/total.sql
@@ -1,5 +1,6 @@
 CREATE VIEW total AS
 SELECT
+    locality,
     SUM(families) AS families,
     SUM(households) AS households,
     SUM(population) AS population,
